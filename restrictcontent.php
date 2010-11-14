53767d572f9ee48ba3a1ef5329353b4d12e656e1
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-14 22:15:02 +0000

    Better Description
    git-svn-id: https://plugins.svn.wordpress.org/restrict-content/trunk@311434 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- restrictcontent.php
+++ restrictcontent.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Restrict Content
-Plugin URI: http://pippinspages.com/freebies/restricted-content-plugin-free/
+Plugin URL: http://pippinspages.com/freebies/restricted-content-plugin-free/
 Description: Restrict Content to registered users only. This is a simple plugin that will allow you to easily restrict complete posts / pages to logged in users only. Levels of restriction may also be set. For example, you can restrict content to only Administrators, Editors, Authors, and Subscribers.
 
 This plugin will also allow you to restrict sections of content within a post or page.
 
 Version: 1.2
 Author: Pippin Williamson
-Author URL: http://pippinspages.com
+Author URI: http://pippinspages.com
 Tags: Restrict content, member only, registered, logged in
 */
 
